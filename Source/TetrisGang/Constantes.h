8fd5d7bf3455d15a62a256d957ed17cfbabf9ec5
ruliii <raul.corrales.avila@gmail.com>
2023-10-20 23:33:48 +0200

    Enums de rotacion y de piezas, quitada gravedad al proyectil

---
+++ Source/TetrisGang/Constantes.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+
+
+UENUM()
+enum Rotations{
+	Up,
+	Down,
+	Left,
+	Right
+};
+
+UENUM()
+enum Pieces{
+	Yelow,
+	Cyan,
+	Green,
+	Red,
+	Orange,
+	Blue,
+	Purple
+};
+
+/**
+ * 
+ */
+class TETRISGANG_API Constantes
+{
+public:
+
+};
