3745b03e3c8ca8db3b7aa4525f1bb677ab6d7fd5
alvcabre <alvarocabrerobarros@gmail.com>
2023-10-20 23:04:23 +0200

    air enemy test

---
+++ Source/TetrisGang/Enemies/AirController.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "AirController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TETRISGANG_API AAirController : public AAIController
+{
+	GENERATED_BODY()
+
+	//bool ShouldPostponePathUpdates() const override;
+};
