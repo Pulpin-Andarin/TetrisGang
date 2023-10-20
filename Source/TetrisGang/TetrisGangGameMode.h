b8f9006c40aaf49005f6775ce385d18fff51bd69
alvcabre <alvarocabrerobarros@gmail.com>
2023-10-20 20:02:05 +0200

    Initial commit

---
+++ Source/TetrisGang/TetrisGangGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TetrisGangGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATetrisGangGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATetrisGangGameMode();
+};
+
+
+
